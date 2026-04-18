a0926f98da4070b67bc283d051c2e1c3270c6649
basmala-mohamed22 <basmalacs25@gmail.com>
2026-04-18 14:49:16 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,131 @@
-namespace oop_Project
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_frist_app
 {
-    internal static class Program
+    public partial class Form2 : Form
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form2()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormLogIn());
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FlowLayoutPanel flp = new FlowLayoutPanel();
+            TextBox tet1 = new TextBox();
+            TextBox tet2 = new TextBox();
+            TextBox tet3 = new TextBox();
+            TextBox tet4 = new TextBox();
+            TextBox tet5 = new TextBox();
+            CheckBox cbx1 = new CheckBox();
+            CheckBox cbx2 = new CheckBox();
+            CheckBox cbx3 = new CheckBox();
+            CheckBox cbx4 = new CheckBox();
+            CheckBox cbx5 = new CheckBox();
+            Control[] allc = { tet1, tet2, tet3, tet4, tet5, cbx1, cbx2, cbx3, cbx4, cbx5 };
+
+
+
+
+
+
+
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 Thrform = new Form3();
+            Thrform.Show();
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
